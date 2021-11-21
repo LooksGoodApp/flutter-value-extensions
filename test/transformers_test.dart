4e0a502ecc7f472dfc6ffa7b1e90e52bd6ad2e21
Yakov Karpov <yakovkarpoff@gmail.com>
2021-11-21 05:40:29 +0300

    Fix map tests

--- test/transformers_test.dart
+++ test/transformers_test.dart
@@ -1,59 +1,56 @@
+// ignore_for_file: cascade_invocations
+
 import 'package:flutter/material.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 import 'package:value_extensions/value_extensions.dart';
 
-void _transformerTest({
-  required String name,
-  required VoidCallback withoutListening,
-  required VoidCallback withListening,
-  required VoidCallback unsubscribing,
-}) =>
-    group(
-      "$name transformer tests",
-      () {
-        test("Changing value without listening", withoutListening);
-        test("Changing value with listening", withoutListening);
-        test("Changing value and changing subscription status",
-            withoutListening);
-      },
-    );
-
 void main() {
   int timesTwo(int x) => x * 2;
 
-  _transformerTest(
-    name: "Map",
-    withoutListening: () {
-      final source = ValueNotifier(1);
-      final mapped = source.map(timesTwo);
-      expect(mapped.value, 2);
-      source.set(2);
-      expect(mapped.value, 4);
-      source.dispose();
-    },
-    withListening: () {
-      final source = ValueNotifier(1);
-      final mapped = source.map(timesTwo)..subscribe((_) {});
-      expect(mapped.value, 2);
-      source.set(2);
-      expect(mapped.value, 4);
-      source.dispose();
-    },
-    unsubscribing: () {
-      final source = ValueNotifier(1);
-      final mapped = source.map(timesTwo);
-      final subscription = mapped.subscribe((_) {});
-      expect(mapped.value, 2);
-      source.set(2);
-      expect(mapped.value, 4);
-      subscription.pause();
-      source.set(3);
-      expect(mapped.value, 6);
-      subscription.pause();
-      source.set(2);
-      expect(mapped.value, 4);
-      source.dispose();
+  group(
+    "Map transformer tests",
+    () {
+      test("Changing value without listening", () {
+        final source = ValueNotifier(1);
+        final mapped = source.map(timesTwo);
+        expect(mapped.value, 2);
+        source.set(2);
+        expect(mapped.value, 4);
+        source.dispose();
+      });
+      test("Changing value with listening", () {
+        final source = ValueNotifier(1);
+        final mapped = source.map(timesTwo)..subscribe((_) {});
+        expect(mapped.value, 2);
+        source.set(2);
+        expect(mapped.value, 4);
+        source.dispose();
+      });
+      test("Changing value and changing subscription status", () {
+        var invoked = 0;
+
+        int timesTwoCounting(int x) {
+          invoked++;
+          return x * 2;
+        }
+
+        final source = ValueNotifier(1);
+        final mapped = source.map(timesTwoCounting); // invoked = 1
+        final subscription = mapped.subscribe((_) {}); // invoked = 2
+        expect(mapped.value, 2);
+        expect(invoked, 2);
+        source.set(2); // invoked = 3
+        expect(mapped.value, 4);
+        subscription.pause();
+        source.set(3);
+        expect(mapped.value, 6); // invoked = 4
+        source.set(100); // No effect on invoked – not read and not listened to
+        subscription.pause(); // invoked = 5
+        source.set(2); // invoked = 6
+        expect(invoked, 6);
+        source.dispose();
+      });
     },
   );
 }
